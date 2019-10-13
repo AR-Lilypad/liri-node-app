e0a90a830f739c643985c0eba4c9f3e032de6c01
AR-Lilypad <angie-for-seekers@msn.com>
2019-10-12 21:30:39 -0400

    obtained spotify keys, pseudo-code 'liri.js'

--- liri.js
+++ liri.js
@@ -1,4 +1,20 @@
-require("dotenv").config();
+// add code to read and set any environment variables with the dotenv package
+var dotenv = require("dotenv").config();
 
+// Add the code required to import the `keys.js` file and store it in a variable
 var keys = require("./keys.js");
+
+// access your keys 
 var spotify = new Spotify(keys.spotify);
+
+// Pseudo code - next steps
+// Make it so liri.js can take in one of the following commands:
+
+// * `concert-this`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
+

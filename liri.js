3671cdf7483ee872bb8a4d8d8107b4083d912303
Angie Roach <angieroach4@gmail.com>
2019-10-24 19:03:20 -0400

    updated concert-this code

--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
 // add code to read and set any environment variables with the dotenv package
-// require("dotenv").config();
+require("dotenv").config();
 
 // the requireds
 let axios = require("axios");
-// let Spotify = require("'node-spotify-api'");
+let Spotify = require("'node-spotify-api'");
 let moment = require("moment");
-// let fs = require("fs");
+let fs = require("fs");
 
 // Add the code required to import the `keys.js` file and store it in a variable
 // clear
-// const keys = require("./keys.js");
+const keys = require("./keys.js");
 
 // access your keys 
-// let spotify = new Spotify(keys.spotify);
+let spotify = new Spotify(keys.spotify);
 // let omdbKey = keys.omdb.api_key;
 
 
@@ -20,73 +20,89 @@ let moment = require("moment");
 
 // Make it so liri.js can take in one of the following commands:
 
-
 // * `concert-this`
 // * `spotify-this-song`
 // * `movie-this`
 // * `do-what-it-says`
 
-let selection = process.argv[2];
-let userInput = process.argv[3];
+//variables for arguments that users will input on the command line selection will run the specific package
+// let selection = process.argv[2];
+// // and userInput will search for the user's query
+// let userInput = process.argv.slice(3).join(" ");
 
-switch (selection) {
-    case "concert-this":
-        concertThis();
-        break;
-    // case "spotify-this-song":
-    //     spotifyThisSong(value);
-    //     break;                          //     if (userSelect) {
-    //         spotifyThisSong(userSelect)
-    //     } else {
-    //         spotifyThisSong("A Love Supreme")
-    //     }
-    // case "movie-this":
-    //     movieThis(value);               //     if (userSelect) {
-    //     break;                          //         omdb(userSelect);
-    //                                     //     } else {
-    //                                     //         omdb("Mr. Nobody");
-    //                                     //     }
-    // case "do-what-it-says":
-    //     doWhatItSays(value);
-    //     break;
+var userSelect = function (userSelection, entertainment) {
+    switch (userSelection) {
+        case "concert-this":
+            concertInfo(entertainment);
 
-
-    default:
-        console.log("Type a value to search such a band, a movie, or spotify something.")
-};
+        // case "spotify-this-song":
+        //     spotifyThisSong(value);
+        //     break;                          //     if (userSelect) {
+        //         spotifyThisSong(userSelect)
+        //     } else {
+        //         spotifyThisSong("A Love Supreme")
+        //     }
+        // case "movie-this":
+        //     movieThis(value);               //     if (userSelect) {
+        //     break;                          //         omdb(userSelect);
+        //                                     //     } else {
+        //                                     //         omdb("Mr. Nobody");
+        //                                     //     }
+        // case "do-what-it-says":
+        //     doWhatItSays(value);
+        //     break;
 
 
+        // default:
+        //     console.log("Type a value to search such as a band, a movie, or to 'spotify' some piece of music or artist.")
+    }
+}
+
+// var concertInfo = function (artist) {
+//     var queryurl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+//     axios.get(queryurl)
+//         .then(function (response) {
+//             console.log(response.data);
+//         })
+// }
 // concert-this 
-function concertThis (selection, userInput) {
-    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+var concertInfo = function (artist) {
+    console.log(artist);
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
         .then(function (response) {
-            console.log(response);
-            let eventList = response.data;
-            eventList.forEach(function (response) {
-                let dateFormat = moment(response.datetime).format("MM/DD/YYYY");
-            })
-            console.log(`${response.venue.name},
-            ${response.city}, ${response.region},${response.datetime}
-=====================================================================`);
+            // for (var i = 0; i < eventList.length; i++) {
+            
+            console.log("=======================================================================")
+            console.log("Venue Name: " + response.data[0].venue.name + "\n");
+            console.log("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + "\n");
+            console.log("Concert Date : " + moment(response.data[0].datetime).format("MM/DD/YY") + "\n");
 
-            err => {
-                if (err) console.log("error.response");
-            }
+            //array for the log.txt
+            let eventList = response.data;
+            fs.appendFile("log.txt", response.data[0].venue.name + response.data[0].venue.city + response.data[0].venue.region + response.data[0].datetime + "\n", function (error) {
+                if (error) {
+                    return console.log(error);
+                }
+            });
         })
 }
- 
+
+
+
+    // =====================================================================
+
 
 
 // spotify
-// function spotifyThisSong(song) {
-//     spotify.search({ type: 'track', query: "Love Supreme" })
-//       .then(function (response) {
-//     console.log(response);
-// })
-//     .catch(function (err) {
-//         console.log(err);
-//     });
-// }
+function spotifyThisSong(song) {
+    spotify.search({ type: 'track', query: "Love Supreme" })
+      .then(function (response) {
+    console.log(response);
+})
+    .catch(function (err) {
+        console.log(err);
+    });
+}
 
 // omdb key = 
 // const BASE_URL = `https://www.omdbapi.com/?apikey=${process.env.VUE_APP_OMDB_KEY}`
@@ -132,4 +148,8 @@ function concertThis (selection, userInput) {
 //             console.log(output[i]);
 //         }
 //     });
-// }
+// 
+var runAll = function (userSelection, entertainment) {
+    userSelect(userSelection, entertainment);
+}
+runAll(process.argv[2], process.argv.slice(3).join(" "));
